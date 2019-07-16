1c457921ebdf7740a5e367a35a4cc7670a0d73d5
dianaRamosAg <52227360+dianaRamosAg@users.noreply.github.com>
2019-07-16 16:41:19 -0500

    RANK

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -9,25 +9,25 @@ export class PostService {
       name: 'Tulipanes',
       description: 'Ramo de tulipanes',
       img:"https://i1.wp.com/www.tecnicomo.com/wp-content/uploads/2017/03/Las-mejores-p%C3%A1ginas-web-para-ordenar-flores-online-9.jpg?resize=333%2C250",
-      rank:"1"
+      rank:"star"
     },
     {
       name: 'Motocicleta',
       description: 'Motocicleta azul',
       img:"https://cdn.xl.thumbs.canstockphoto.es/moto-icono-clip-art-vectorial_csp43571662.jpg",
-      rank:"2"
+      rank:"star"
     },
     {
       name: 'Multiplataforma',
       description: 'Tablet y celular',
       img:"https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/styles/480/public/media/image/2014/10/67621-jimdo-app-crear-tu-propia-pagina-web-smartphone-tablet.jpg?itok=1EeHAlsf",
-      rank:"3"
+      rank:"star"
     },
     {
       name: 'Gorra',
       description: 'Gorra',
       img:"https://image.jimcdn.com/app/cms/image/transf/none/path/s36658c87bf479136/image/i9040edc4a250d572/version/1487605795/image.jpg",
-      rank:"4"
+      rank:"star"
     }
 ];
 
